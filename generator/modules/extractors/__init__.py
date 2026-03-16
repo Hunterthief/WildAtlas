7380683a606a682741c1e1092d797ab7b2e94ebd
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-03-16 23:49:57 +0000

    Update __init__.py

--- generator/modules/extractors/__init__.py
+++ generator/modules/extractors/__init__.py
@@ -1,4 +1,4 @@
-# generator/extractors/__init__.py
+# generator/modules/extractors/__init__.py
 from .sections import extract_wikipedia_sections
 from .stats import extract_stats_from_sections
 from .diet import extract_diet_from_sections
@@ -6,13 +6,19 @@ from .reproduction import extract_reproduction_from_sections
 from .conservation import extract_conservation_from_sections
 from .behavior import extract_behavior_from_sections
 from .additional_info import extract_additional_info_from_sections
+from .wikidata_enhancer import extract_wikidata_all
+from .gbif_distribution import extract_gbif_all
+from .eol_data import extract_eol_all
 
 __all__ = [
-    'extract_wikipedia_sections',
-    'extract_stats_from_sections',
-    'extract_diet_from_sections',
-    'extract_reproduction_from_sections',
-    'extract_conservation_from_sections',
-    'extract_behavior_from_sections',
-    'extract_additional_info_from_sections'
+    "extract_wikipedia_sections",
+    "extract_stats_from_sections",
+    "extract_diet_from_sections",
+    "extract_reproduction_from_sections",
+    "extract_conservation_from_sections",
+    "extract_behavior_from_sections",
+    "extract_additional_info_from_sections",
+    "extract_wikidata_all",
+    "extract_gbif_all",
+    "extract_eol_all"
 ]
