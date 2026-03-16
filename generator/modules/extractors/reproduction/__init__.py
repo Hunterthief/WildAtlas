9008c1d5dec5952c620d8055eb36d4ee347bfe3d
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-03-16 23:40:37 +0000

    Delete generator/modules/extractors/reproduction directory

--- generator/modules/extractors/reproduction/__init__.py
+++
@@ -1,7 +0,0 @@
-# generator/modules/extractors/reproduction/__init__.py
-"""Reproduction extraction functions"""
-
-from .gestation import extract_gestation
-from .litter_size import extract_litter_size
-
-__all__ = ['extract_gestation', 'extract_litter_size']
