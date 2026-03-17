63d05d2a76fbcd6dabbd65a6aa6a0330cf83b66f
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-03-17 22:14:02 +0000

    Update __init__.py

--- generator/modules/fetchers/__init__.py
+++ generator/modules/fetchers/__init__.py
@@ -1,1 +1,9 @@
+# generator/modules/fetchers/__init__.py
+from .wikipedia import fetch_wikipedia_summary, fetch_wikipedia_full
+from .inaturalist import fetch_inaturalist
 
+__all__ = [
+    'fetch_wikipedia_summary',
+    'fetch_wikipedia_full',
+    'fetch_inaturalist'
+]
