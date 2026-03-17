7a498d9056f8a1c002f7dcc60c416cb269572631
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-03-17 23:16:25 +0000

    Update __init__.py

--- generator/modules/fetchers/__init__.py
+++ generator/modules/fetchers/__init__.py
@@ -1,9 +1,22 @@
-# generator/modules/fetchers/__init__.py
+"""
+Data Fetchers - All external API/data source fetchers
+"""
+
+from .api_ninjas import fetch_animal_data
 from .wikipedia import fetch_wikipedia_summary, fetch_wikipedia_full
 from .inaturalist import fetch_inaturalist
+from .wikidata import fetch_wikidata_properties
+from .gbif_distribution import fetch_gbif_distribution
+from .eol_data import fetch_eol_data
+from .iucn_redlist import fetch_iucn_status
 
 __all__ = [
+    'fetch_animal_data',
     'fetch_wikipedia_summary',
     'fetch_wikipedia_full',
-    'fetch_inaturalist'
+    'fetch_inaturalist',
+    'fetch_wikidata_properties',
+    'fetch_gbif_distribution',
+    'fetch_eol_data',
+    'fetch_iucn_status'
 ]
