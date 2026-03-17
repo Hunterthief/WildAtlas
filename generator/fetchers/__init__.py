979d5e2b443807b6043c4a21169f5defb23fa80d
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-03-17 22:26:22 +0000

    Delete generator/fetchers directory

--- generator/fetchers/__init__.py
+++
@@ -1,9 +0,0 @@
-# generator/fetchers/__init__.py
-from .wikipedia import fetch_wikipedia_summary, fetch_wikipedia_full
-from .inaturalist import fetch_inaturalist
-
-__all__ = [
-    'fetch_wikipedia_summary',
-    'fetch_wikipedia_full',
-    'fetch_inaturalist'
-]
