ed99093c8d47599b8787ff0ea2948a5e24a91b03
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-06-26 02:10:01 +0000

    Delete generator directory

--- generator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-WildAtlas Generator Package
-Animal data generation from multiple sources
-"""
-
-__version__ = "1.0.0"
-__author__ = "Hunterthief"
--- generator/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Modules Package - Core functionality for WildAtlas
-"""
-
-__version__ = "1.0.0"
